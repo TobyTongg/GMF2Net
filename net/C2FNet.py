c4bc5a4f7c16a8fce2005407dba4cb931d21a17c
Xuwei Tong <105258505+TobyTongg@users.noreply.github.com>
2023-07-28 14:40:19 +0800

    Update C2FNet.py

--- net/C2FNet.py
+++ net/C2FNet.py
@@ -1,1 +1,490 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import imageio
+import numpy
+from libs.Res2Net_v1b import res2net50_v1b_26w_4s
+from libs.Res2Net_v1b import res2net101_v1b_26w_4s
+from utils.tensor_ops import cus_sample, upsample_add
 
+
+class BasicConv2d(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1, relu=False, bn=True):
+        super(BasicConv2d, self).__init__()
+        self.conv = nn.Conv2d(in_planes, out_planes,
+                              kernel_size=kernel_size, stride=stride,
+                              padding=padding, dilation=dilation, bias=False)
+        self.bn = nn.BatchNorm2d(out_planes) if bn else None
+        self.relu = nn.ReLU(inplace=True) if relu else None
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
+
+
+class RFB_modified(nn.Module):
+    def __init__(self, in_channel, out_channel):
+        super(RFB_modified, self).__init__()
+        self.relu = nn.ReLU(True)
+        self.branch0 = nn.Sequential(
+            BasicConv2d(in_channel, out_channel, 1),
+        )
+        self.branch1 = nn.Sequential(
+            BasicConv2d(in_channel, out_channel, 1),
+            BasicConv2d(out_channel, out_channel, kernel_size=(1, 3), padding=(0, 1)),
+            BasicConv2d(out_channel, out_channel, kernel_size=(3, 1), padding=(1, 0)),
+            BasicConv2d(out_channel, out_channel, 3, padding=3, dilation=3)
+        )
+        self.branch2 = nn.Sequential(
+            BasicConv2d(in_channel, out_channel, 1),
+            BasicConv2d(out_channel, out_channel, kernel_size=(1, 5), padding=(0, 2)),
+            BasicConv2d(out_channel, out_channel, kernel_size=(5, 1), padding=(2, 0)),
+            BasicConv2d(out_channel, out_channel, 3, padding=5, dilation=5)
+        )
+        self.branch3 = nn.Sequential(
+            BasicConv2d(in_channel, out_channel, 1),
+            BasicConv2d(out_channel, out_channel, kernel_size=(1, 7), padding=(0, 3)),
+            BasicConv2d(out_channel, out_channel, kernel_size=(7, 1), padding=(3, 0)),
+            BasicConv2d(out_channel, out_channel, 3, padding=7, dilation=7)
+        )
+        self.conv_cat = BasicConv2d(4*out_channel, out_channel, 3, padding=1)
+        self.conv_res = BasicConv2d(in_channel, out_channel, 1)
+
+    def forward(self, x):
+        x0 = self.branch0(x)
+        x1 = self.branch1(x)
+        x2 = self.branch2(x)
+        x3 = self.branch3(x)
+        x_cat = self.conv_cat(torch.cat((x0, x1, x2, x3), 1))
+
+        x = self.relu(x_cat + self.conv_res(x))
+        return x
+
+
+class MSCA(nn.Module):
+    def __init__(self, channels=64, r=4):
+        super(MSCA, self).__init__()
+        out_channels = int(channels // r)
+        # local_att
+        self.local_att = nn.Sequential(
+            nn.Conv2d(channels, out_channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(channels)
+        )
+
+        # global_att
+        self.global_att = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(channels, out_channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(channels)
+        )
+
+        self.sig = nn.Sigmoid()
+
+    def forward(self, x):
+        xl = self.local_att(x)
+        xg = self.global_att(x)
+        xlg = xl + xg
+        wei = self.sig(xlg)
+
+        return wei
+
+
+
+class PPM(nn.Module):
+    def __init__(self, in_channel, out_channel, **kwargs):
+        super(PPM, self).__init__()
+        interchannel = int(in_channel / 4)
+        self.conv1 = nn.Conv2d(in_channel, interchannel, 1, **kwargs)
+        self.conv2 = nn.Conv2d(in_channel, interchannel, 1, **kwargs)
+        self.conv3 = nn.Conv2d(in_channel, interchannel, 1, **kwargs)
+        self.conv4 = nn.Conv2d(in_channel, interchannel, 1, **kwargs)
+        self.out = nn.Conv2d(in_channel * 2, out_channel, 1)
+
+
+
+    def pool(self, x, size):
+        avge = nn.AdaptiveAvgPool2d(size)
+        return avge(x)
+
+    def upsample(self, x, size):
+
+        return F.interpolate(x, size, mode='bilinear', align_corners=True)
+
+    def forward(self, x):
+        x_size = x.size()
+        x_size = torch.tensor(x_size)
+        x_size = x_size.numpy().tolist()
+        size = x_size[2:]
+
+
+        interout1 = self.pool(x, 1)
+        interout2 = self.pool(x, 2)
+        interout3 = self.pool(x, 3)
+        interout6 = self.pool(x, 6)
+
+        out1 = self.conv1(interout1)
+        out2 = self.conv2(interout2)
+        out3 = self.conv3(interout3)
+        out6 = self.conv4(interout6)
+
+        out1_1 = self.upsample(out1,size)
+        out2_1 = self.upsample(out2,size)
+        out3_1 = self.upsample(out3,size)
+        out6_1 = self.upsample(out6,size)
+
+
+
+        out5 = torch.cat([x, out1_1, out2_1, out3_1, out6_1], dim=1)
+        out = self.out(out5)
+        return out
+
+
+
+class ACFM(nn.Module):
+    def __init__(self, channel=64):
+        super(ACFM, self).__init__()
+
+        self.msca = MSCA()
+        self.upsample = cus_sample
+        self.conv = BasicConv2d(channel, channel, kernel_size=3, stride=1, padding=1, relu=True)
+
+    def forward(self, x, y):
+        y = self.upsample(y, scale_factor=2)
+        xy = x + y
+        wei = self.msca(xy)
+        xo = x * wei + y * (1 - wei)
+        xo = self.conv(xo)
+
+        return xo
+
+
+class DGCM(nn.Module):
+    def __init__(self, channel=64):
+        super(DGCM, self).__init__()
+        self.h2l_pool = nn.AvgPool2d((2, 2), stride=2)
+
+        self.h2l = BasicConv2d(channel, channel, kernel_size=3, stride=1, padding=1, relu=True)
+        self.h2h = BasicConv2d(channel, channel, kernel_size=3, stride=1, padding=1, relu=True)
+
+        self.mscah = MSCA()
+        self.mscal = MSCA()
+        self.upsample_add = upsample_add
+        self.conv = BasicConv2d(channel, channel, kernel_size=3, stride=1, padding=1, relu=True)
+
+    def forward(self, x):
+        # first conv
+        x_h = self.h2h(x)
+
+        x_l = self.h2l(self.h2l_pool(x))
+        x_h = x_h * self.mscah(x_h)
+        x_l = x_l * self.mscal(x_l)
+        out = self.upsample_add(x_l, x_h)
+        out = self.conv(out)
+
+        return out
+
+class Context_Exploration_Block(nn.Module):
+    def __init__(self, input_channels):
+        super(Context_Exploration_Block, self).__init__()
+        self.input_channels = input_channels
+        self.channels_single = int(input_channels / 4)
+
+        self.p1_channel_reduction = nn.Sequential(
+            nn.Conv2d(self.input_channels, self.channels_single, 1, 1, 0),
+            nn.BatchNorm2d(self.channels_single), nn.ReLU())
+        self.p2_channel_reduction = nn.Sequential(
+            nn.Conv2d(self.input_channels, self.channels_single, 1, 1, 0),
+            nn.BatchNorm2d(self.channels_single), nn.ReLU())
+        self.p3_channel_reduction = nn.Sequential(
+            nn.Conv2d(self.input_channels, self.channels_single, 1, 1, 0),
+            nn.BatchNorm2d(self.channels_single), nn.ReLU())
+        self.p4_channel_reduction = nn.Sequential(
+            nn.Conv2d(self.input_channels, self.channels_single, 1, 1, 0),
+            nn.BatchNorm2d(self.channels_single), nn.ReLU())
+
+        self.p1 = nn.Sequential(
+            nn.Conv2d(self.channels_single, self.channels_single, 1, 1, 0),
+            nn.BatchNorm2d(self.channels_single), nn.ReLU())
+        self.p1_dc = nn.Sequential(
+            nn.Conv2d(self.channels_single, self.channels_single, kernel_size=3, stride=1, padding=1, dilation=1),
+            nn.BatchNorm2d(self.channels_single), nn.ReLU())
+
+        self.p2 = nn.Sequential(
+            nn.Conv2d(self.channels_single, self.channels_single, 3, 1, 1),
+            nn.BatchNorm2d(self.channels_single), nn.ReLU())
+        self.p2_dc = nn.Sequential(
+            nn.Conv2d(self.channels_single, self.channels_single, kernel_size=3, stride=1, padding=2, dilation=2),
+            nn.BatchNorm2d(self.channels_single), nn.ReLU())
+
+        self.p3 = nn.Sequential(
+            nn.Conv2d(self.channels_single, self.channels_single, 5, 1, 2),
+            nn.BatchNorm2d(self.channels_single), nn.ReLU())
+        self.p3_dc = nn.Sequential(
+            nn.Conv2d(self.channels_single, self.channels_single, kernel_size=3, stride=1, padding=4, dilation=4),
+            nn.BatchNorm2d(self.channels_single), nn.ReLU())
+
+        self.p4 = nn.Sequential(
+            nn.Conv2d(self.channels_single, self.channels_single, 7, 1, 3),
+            nn.BatchNorm2d(self.channels_single), nn.ReLU())
+        self.p4_dc = nn.Sequential(
+            nn.Conv2d(self.channels_single, self.channels_single, kernel_size=3, stride=1, padding=8, dilation=8),
+            nn.BatchNorm2d(self.channels_single), nn.ReLU())
+
+        self.fusion = nn.Sequential(nn.Conv2d(self.input_channels, self.input_channels, 1, 1, 0),
+                                    nn.BatchNorm2d(self.input_channels), nn.ReLU())
+
+    def forward(self, x):
+        p1_input = self.p1_channel_reduction(x)
+        p1 = self.p1(p1_input)
+        p1_dc = self.p1_dc(p1)
+
+        p2_input = self.p2_channel_reduction(x) + p1_dc
+        p2 = self.p2(p2_input)
+        p2_dc = self.p2_dc(p2)
+
+        p3_input = self.p3_channel_reduction(x) + p2_dc
+        p3 = self.p3(p3_input)
+        p3_dc = self.p3_dc(p3)
+
+        p4_input = self.p4_channel_reduction(x) + p3_dc
+        p4 = self.p4(p4_input)
+        p4_dc = self.p4_dc(p4)
+
+        ce = self.fusion(torch.cat((p1_dc, p2_dc, p3_dc, p4_dc), 1))
+
+        return ce
+
+class CA_Block(nn.Module):
+    def __init__(self, in_dim):
+        super(CA_Block, self).__init__()
+        self.chanel_in = in_dim
+        self.gamma = nn.Parameter(torch.ones(1))
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        """
+            inputs :
+                x : input feature maps (B X C X H X W)
+            returns :
+                out : channel attentive features
+        """
+        m_batchsize, C, height, width = x.size()
+        proj_query = x.view(m_batchsize, C, -1)
+        proj_key = x.view(m_batchsize, C, -1).permute(0, 2, 1)
+        energy = torch.bmm(proj_query, proj_key)
+        attention = self.softmax(energy)
+        proj_value = x.view(m_batchsize, C, -1)
+
+        out = torch.bmm(attention, proj_value)
+        out = out.view(m_batchsize, C, height, width)
+
+        out = self.gamma * out + x
+        return out
+
+class SA_Block(nn.Module):
+    def __init__(self, in_dim):
+        super(SA_Block, self).__init__()
+        self.chanel_in = in_dim
+        self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
+        self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
+        self.value_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+        self.gamma = nn.Parameter(torch.ones(1))
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        """
+            inputs :
+                x : input feature maps (B X C X H X W)
+            returns :
+                out : spatial attentive features
+        """
+        m_batchsize, C, height, width = x.size()
+        proj_query = self.query_conv(x).view(m_batchsize, -1, width * height).permute(0, 2, 1)
+        proj_key = self.key_conv(x).view(m_batchsize, -1, width * height)
+        energy = torch.bmm(proj_query, proj_key)
+        attention = self.softmax(energy)
+        proj_value = self.value_conv(x).view(m_batchsize, -1, width * height)
+
+        out = torch.bmm(proj_value, attention.permute(0, 2, 1))
+        out = out.view(m_batchsize, C, height, width)
+
+        out = self.gamma * out + x
+        return out
+
+class Positioning(nn.Module):
+    def __init__(self, channel):
+        super(Positioning, self).__init__()
+        self.channel = channel
+        self.cab = CA_Block(self.channel)
+        self.sab = SA_Block(self.channel)
+        self.map = nn.Conv2d(self.channel, 1, 7, 1, 3)
+
+    def forward(self, x):
+        cab = self.cab(x)
+        sab = self.sab(cab)
+        map = self.map(sab)
+
+        return sab, map
+
+class Focus(nn.Module):
+    def __init__(self, channel1, channel2):
+        super(Focus, self).__init__()
+        self.channel1 = channel1
+        self.channel2 = channel2
+
+        self.up = nn.Sequential(nn.Conv2d(self.channel2, self.channel1, 7, 1, 3),
+                                nn.BatchNorm2d(self.channel1), nn.ReLU(), nn.UpsamplingBilinear2d(scale_factor=2))
+
+        self.input_map = nn.Sequential(nn.UpsamplingBilinear2d(scale_factor=2), nn.Sigmoid())
+        self.output_map = nn.Conv2d(self.channel1, 1, 7, 1, 3)
+
+        self.fp = Context_Exploration_Block(self.channel1)
+        self.fn = Context_Exploration_Block(self.channel1)
+        self.alpha = nn.Parameter(torch.ones(1))
+        self.beta = nn.Parameter(torch.ones(1))
+        self.bn1 = nn.BatchNorm2d(self.channel1)
+        self.relu1 = nn.ReLU()
+        self.bn2 = nn.BatchNorm2d(self.channel1)
+        self.relu2 = nn.ReLU()
+
+    def forward(self, x, y, in_map):
+        # x; current-level features
+        # y: higher-level features
+        # in_map: higher-level prediction
+
+        #up = self.up(y)
+
+
+        input_map = self.input_map(in_map)
+        f_feature = x * input_map
+        b_feature = x * (1 - input_map)
+
+        fp = self.fp(f_feature)
+        fn = self.fn(b_feature)
+
+        refine1 = y - (self.alpha * fp)
+        refine1 = self.bn1(refine1)
+        refine1 = self.relu1(refine1)
+
+        refine2 = refine1 + (self.beta * fn)
+        refine2 = self.bn2(refine2)
+        refine2 = self.relu2(refine2)
+
+        output_map = self.output_map(refine2)
+
+        return refine2, output_map
+
+class C2FNet(nn.Module):
+    # res2net based encoder decoder
+    def __init__(self, channel=64):
+        super(C2FNet, self).__init__()
+        # ---- ResNet Backbone ----
+        self.resnet = res2net50_v1b_26w_4s(pretrained=True)
+        # ---- Receptive Field Block like module ----
+        self.rfb1_1 = RFB_modified(256, channel)
+        self.rfb2_1 = RFB_modified(512, channel)
+        self.rfb3_1 = RFB_modified(1024, channel)
+        self.rfb4_1 = RFB_modified(2048, channel)
+        self.ppm = PPM(2048,64)
+
+        self.position = Positioning(64)
+
+        self.focus43 = Focus(64,64)
+
+
+
+
+        self.acfm3 = ACFM()
+        self.acfm2 = ACFM()
+        self.acfm1 = ACFM()
+        self.acfm0 = ACFM()
+
+        self.dgcm3 = DGCM()
+        self.dgcm2 = DGCM()
+        self.dgcm1 = DGCM()
+        self.dgcm0 = DGCM()
+
+        self.upconv3 = BasicConv2d(64, 64, kernel_size=3, stride=1, padding=1, relu=True)
+        self.upconv2 = BasicConv2d(64, 64, kernel_size=3, stride=1, padding=1, relu=True)
+
+
+        self.classifier = nn.Conv2d(64, 1, 1)
+        self.upsample_add = upsample_add
+
+        # if self.training:l
+        # self.initialize_weights()
+
+    def forward(self, x):
+        x = self.resnet.conv1(x)
+        x = self.resnet.bn1(x)
+        x = self.resnet.relu(x)
+        x = self.resnet.maxpool(x)  # bs, 64, 88, 88
+        # ---- low-level features ----
+        x1 = self.resnet.layer1(x)  # bs, 256, 88, 88
+        x2 = self.resnet.layer2(x1)  # bs, 512, 44, 44
+        x3 = self.resnet.layer3(x2)  # bs, 1024, 22, 22
+        x4 = self.resnet.layer4(x3)  # bs, 2048, 11, 11
+
+        ppm = self.ppm(x4)
+
+
+        x1_rfb = self.rfb1_1(x1)  # channel -> 64
+        x2_rfb = self.rfb2_1(x2)  # channel -> 64
+        x3_rfb = self.rfb3_1(x3)  # channel -> 64
+        x4_rfb = self.rfb4_1(x4)  # channel -> 64
+
+        position4, pre4 = self.position(x4_rfb)
+        x43 = self.acfm3(x3_rfb, position4)
+        x43 = self.upsample_add(ppm, x43)
+        dgcm43 = self.dgcm3(x43)
+
+        focus43, pre43 = self.focus43(x3_rfb, dgcm43, pre4)
+        out43 = self.upconv3(self.dgcm3(x43) + focus43)
+
+
+
+        x432 = self.acfm2(x2_rfb, out43)
+        x432 = self.upsample_add(ppm, x432)
+        out432 = self.upconv2(self.dgcm2(x432) + x432)
+
+        x4321 = self.acfm1(x1_rfb, out432)
+        x4321 = self.upsample_add(ppm, x4321)
+        out4321 = self.upconv2(self.dgcm1(x4321) + x4321)
+
+        f_out = self.classifier(out4321)
+
+        pre4 = F.interpolate(pre4, scale_factor=32, mode='bilinear', align_corners=True)
+        f_out = F.interpolate(f_out, scale_factor=4, mode='bilinear', align_corners=False)
+
+        return f_out, pre4
+
+    # def initialize_weights(self):
+    # res50 = models.resnet50(pretrained=True)
+    # pretrained_dict = res50.state_dict()
+    # all_params = {}
+    # for k, v in self.resnet.state_dict().items():
+    # if k in pretrained_dict.keys():
+    # v = pretrained_dict[k]
+    # all_params[k] = v
+    # assert len(all_params.keys()) == len(self.resnet.state_dict().keys())
+    # self.resnet.load_state_dict(all_params)
+
+
+if __name__ == '__main__':
+    ras = C2FNet().cuda()
+    input_tensor = torch.randn(2, 3, 352, 352).cuda()
+
+    final_out , pre3= ras(input_tensor)
+
+
+    print(final_out.shape, pre3.shape)
